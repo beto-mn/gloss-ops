9db4e70ff569378725234c55a19e1e12e8c33572
Beto Najera <ing.betonajera@gmail.com>
2026-05-20 18:01:04 -0600

    ✨ feat(warranties): wire module, barrel index, and register in AppModule

---
+++ apps/api/src/warranties/warranties.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common'
+
+import { PrismaModule } from '@prisma'
+
+import { ActivityLogsModule } from '../activity-logs/activity-logs.module'
+import { PrismaWarrantyRepository } from './infrastructure/prisma-warranty.repository'
+import { AssetWarrantiesController } from './asset-warranties.controller'
+import { WorkOrderWarrantiesController } from './work-order-warranties.controller'
+import { WarrantiesController } from './warranties.controller'
+import { WARRANTY_REPOSITORY } from './warranties.tokens'
+import { WarrantyService } from './warranties.service'
+
+@Module({
+  imports: [PrismaModule, ActivityLogsModule],
+  controllers: [
+    WarrantiesController,
+    WorkOrderWarrantiesController,
+    AssetWarrantiesController,
+  ],
+  providers: [
+    { provide: WARRANTY_REPOSITORY, useClass: PrismaWarrantyRepository },
+    WarrantyService,
+  ],
+  exports: [WarrantyService],
+})
+export class WarrantiesModule {}
