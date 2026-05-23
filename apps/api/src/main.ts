0ec2681588ee840adb59e6141685de1df542e844
Beto Najera <ing.betonajera@gmail.com>
2026-05-22 23:11:39 -0600

    ✨ feat(api): enable CORS for frontend origin and rename env var to PORT

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -94,6 +94,7 @@ const DARK_CSS = `
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({ origin: envs.app.frontendUrl, credentials: true })
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
   const config = new DocumentBuilder()
